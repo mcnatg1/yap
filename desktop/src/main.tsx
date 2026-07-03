1dc8b7aeaf0c2f50e366d635847e7679d90c89bd
Grant McNatt <mcnatg1@medtronic.com>
2026-07-02 19:06:36 -0500

    chore: finish dependency maintenance batch

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom/client";
 import App from "@/App";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import "@/styles.css";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TooltipProvider>
